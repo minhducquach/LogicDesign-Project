013ca81ef4b64f181d67b7b22488d54ce4b7379c
minhduc <minhduc16102@gmail.com>
2022-10-28 06:16:07 +0700

    Some improvements

--- Webapp/BE/index.js
+++ Webapp/BE/index.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 
-const port = 3000;
+const port = 2022;
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
--- Webapp/BE/router/routes.js
+++ Webapp/BE/router/routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
+router.get("/api", (req, res) => {
+  res.json("Hello World!");
 });
 
 module.exports = router;
