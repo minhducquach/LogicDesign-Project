e2ad9c2a6bc2c34c489e32b20d04630bcc953dd5
minhduc <minhduc16102@gmail.com>
2022-11-14 11:21:23 +0700

    Fetch

--- Webapp/client/src/App.js
+++ Webapp/client/src/App.js
@@ -1,23 +1,37 @@
-import './components/styles/App.css';
-import Header from './components/component/Header';
-import Intial from './components/component/Intial';
-import Homepage from './components/component/Homepage';
-import { useEffect } from 'react';
+import "./components/styles/App.css";
+import Header from "./components/component/Header";
+import Intial from "./components/component/Intial";
+import Homepage from "./components/component/Homepage";
+import { useEffect } from "react";
+import React from "react";
+import axios from "axios";
 
 function App() {
+  // const [data, setData] = React.useState(null);
 
-  const [data, setData] = React.useState(NULL);
-  
-  useEffect(() => {
-    fetch("/get-entries")
-      .then((res) => res.json())
-      .then((data) => setData(data.message))
-  }, []);  
+  // useEffect(() => {
+  //   fetch("/get-entries")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
+  axios({
+    method: "get",
+    url: "http://localhost:5001/logicdesign-project/us-central1/app/get-entries",
+    // data: {
+    //   message: "Hello",
+    // },
+  })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
   return (
     <div>
-      <Header/>
-      <Homepage/>
+      <Header />
+      <Homepage />
     </div>
   );
 }
