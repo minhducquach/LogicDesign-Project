44b2654fd763424a5557f1fc33c4c880329fb6b3
minhduc <minhduc16102@gmail.com>
2022-11-14 11:19:15 +0700

    Fix connection between FE and BE

--- Webapp/functions/index.js
+++ Webapp/functions/index.js
@@ -2,15 +2,15 @@ const functions = require("firebase-functions");
 const express = require("express");
 const router = require("./routes/routes");
 
-var cors = require('cors');
-const PORT = process.env.PORT || 5001;
+var cors = require("cors");
+// const PORT = process.env.PORT || 5001;
 var app = express();
 
 app.use(cors());
 app.use(router);
-app.get("/get-entries", (req, res) => console.log(res));
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+// app.get("/get-entries", (req, res) => console.log(res));
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
 
 exports.app = functions.https.onRequest(app);
