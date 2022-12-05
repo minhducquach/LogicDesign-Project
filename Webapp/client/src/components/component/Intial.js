86216a8c212ad0561aa364933b02fff6eafcc82f
minhduc <minhduc16102@gmail.com>
2022-12-05 18:17:55 +0700

    Delete API

--- Webapp/client/src/components/component/Intial.js
+++ Webapp/client/src/components/component/Intial.js
@@ -1,25 +1,29 @@
-import axios from 'axios';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Intial () {
-
-    return (
-        <div>
-            <div className="body">
-                <div className="content-1">Hello, your relative's position doesn't show on our WebSite</div>
-                <div className="content-2">Please click here to know!</div>
-                <Link to = "/homepage" id='none'>
-                    <div className='click' onClick={() => {
-                        axios({
-                            method: "post",
-                            url: "",
-                        })
-                    }}>
-                        CLICK HERE
-                    </div>
-                </Link>
-            </div> 
+export default function Intial() {
+  return (
+    <div>
+      <div className="body">
+        <div className="content-1">
+          Hello, your relative's position doesn't show on our WebSite
         </div>
-    );
+        <div className="content-2">Please click here to know!</div>
+        <Link to="/homepage" id="none">
+          <div
+            className="click"
+            onClick={() => {
+              axios({
+                method: "get",
+                url: "http://127.0.0.1:5001/logicdesign-project/us-central1/app/delete-all",
+              });
+            }}
+          >
+            CLICK HERE
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
